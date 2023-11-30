3340035ddbbb972a33fafe62c72d02f77ea15706
antonovmike <antonovma@outlook.com>
2023-11-30 18:49:01 +0400

    first

---
+++ main.py
@@ -0,0 +1,124 @@
+import getpass
+import os
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, GLib
+
+
+def read_window_settings():
+    with open('window_settings') as f:
+        settings = f.read().splitlines()
+    width = int(settings[0].split('=')[1])
+    height = int(settings[1].split('=')[1])
+    return [width, height]
+
+
+def read_play_list():
+    with open('play_list') as f:
+        songs = f.readlines()
+    return [song.strip() for song in songs]
+
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Player")
+        height = read_window_settings()[0]
+        width = read_window_settings()[1]
+        self.set_default_size(width, height)
+
+        grid = Gtk.Grid()
+
+        toolbar = Gtk.Toolbar()
+
+        prev_button = Gtk.ToolButton.new(Gtk.Image.new_from_icon_name("media-skip-backward-symbolic", 16), label="Previous")
+        toolbar.insert(prev_button, 0)
+
+        play_button = Gtk.ToolButton.new(Gtk.Image.new_from_icon_name("media-playback-start-symbolic", 16), label="Play")
+        toolbar.insert(play_button, 1)
+
+        stop_button = Gtk.ToolButton.new(Gtk.Image.new_from_icon_name("media-playback-stop-symbolic", 16), label="Stop")
+        toolbar.insert(stop_button, 2)
+
+        next_button = Gtk.ToolButton.new(Gtk.Image.new_from_icon_name("media-skip-forward-symbolic", 16), label="Next")
+        toolbar.insert(next_button, 3)
+
+        progressbar = Gtk.ToolItem()
+        progressbar.add(Gtk.ProgressBar())
+        toolbar.insert(progressbar, 4)
+
+        random_button = Gtk.ToolButton.new(Gtk.Image.new_from_icon_name("view-refresh-symbolic", 16), label="Random")
+        toolbar.insert(random_button, 5)
+
+        repeat_button = Gtk.ToolButton.new(Gtk.Image.new_from_icon_name("media-playlist-repeat-symbolic", 16), label="Repeat")
+        toolbar.insert(repeat_button, 6)
+
+        file_tree = FileTree()
+
+        playlist = Gtk.TreeView()
+        playlist_store = Gtk.ListStore(str)
+        playlist.set_model(playlist_store)
+
+        playlist_column = Gtk.TreeViewColumn("Playlist", Gtk.CellRendererText(), text=0)
+        playlist.append_column(playlist_column)
+
+        for _ in range(5):
+            playlist_store.append([""])
+
+        paned = Gtk.Paned()
+        paned.pack1(file_tree, True, False)
+        paned.pack2(playlist, True, False)
+
+        grid.attach(toolbar, 0, 0, 1, 1)
+        grid.attach(paned, 0, 1, 1, 1)
+        self.add(grid)
+
+        GLib.idle_add(self.update_paned_position, paned)
+
+    def update_paned_position(self, paned):
+        width = paned.get_allocated_width()
+        paned.set_position(int(width * 0.3))
+
+    def populate_file_store(self, store, path):
+        for item in os.listdir(path):
+            item_fullname = os.path.join(path, item)
+            item_is_folder = os.path.isdir(item_fullname)
+            if item_is_folder:
+                store.append(None, [item, item_fullname])
+                self.populate_file_store(store, item_fullname)
+            else:
+                store.append(None, [item, item_fullname])
+
+
+class FileTree(Gtk.TreeView):
+    def __init__(self):
+        Gtk.TreeView.__init__(self)
+        self.file_store = Gtk.TreeStore(str, str)
+        self.set_model(self.file_store)
+
+        file_column = Gtk.TreeViewColumn("Files", Gtk.CellRendererText(), text=0)
+        file_path_column = Gtk.TreeViewColumn("Path", Gtk.CellRendererText(), text=1)
+        self.append_column(file_column)
+        self.append_column(file_path_column)
+
+        user = getpass.getuser()
+        directory = f"/home/{user}/Music"
+
+        root = self.file_store.append(None, [directory, directory])
+        self.add_directory(root, directory)
+
+    def add_directory(self, parent, path):
+        # Recursively add all files and directories to the TreeStore model
+        for item in os.listdir(path):
+            item_path = os.path.join(path, item)
+            if os.path.isdir(item_path):
+                directory = self.file_store.append(parent, [item, item_path])
+                self.add_directory(directory, item_path)
+            else:
+                self.file_store.append(parent, [item, item_path])
+
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
